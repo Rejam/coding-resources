970ab85b698ba3e8aaedd73d695cfba64e91b91a
adrian <adrianhajdin00@gmail.com>
2018-12-04 12:00:44 +0100

    add google login

--- server/app.js
+++ server/app.js
@@ -6,15 +6,24 @@ const cors = require('cors');
 const passport = require('passport');
 const session = require('express-session');
 const cookieSession = require('cookie-session');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
-app.use(cookieSession({ keys: ['asdadad'] }));
+app.use(cookieParser());
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ['asdadad'],
+  maxAge: 24 * 60 * 60 * 1000,
+}));
+
 app.use(bodyParser.json());
 app.use(session({ secret: 'Secret' }));
 app.use(morgan('combined'));
 app.use(helmet());
 app.use(cors());
+
 app.use(passport.initialize());
 app.use(passport.session());
 
